7905ed13b75d1044ba646d832fa6770c4ee2466f
Johan Strootman <jj.strootman@gmail.com>
2021-06-11 12:25:06 +0200

    Les van 11 juni 2021 - Commentaren in code toegevoegd

--- api/app/Http/RequestHandler.php
+++ api/app/Http/RequestHandler.php
@@ -6,6 +6,13 @@ namespace App\Http;
 
 class RequestHandler
 {
+    /*
+     * Hieronder staan zogenaamde properties
+     * Je kunt properties vergelijken met variabelen
+     * maar dan variabelen die uitsluitend leven in een
+     * object. Door private er voor te zetten zijn deze variabelen
+     * niet bereikbaar voor de wereld buiten deze class.
+     */
     private $routes;
     private $request_type;          // GET, POST, PUT
 
@@ -15,6 +22,12 @@ class RequestHandler
     private $resource;
     private $id;
 
+    /*
+     * Onderstaande code wordt uitgevoerd zodra ik met de opdracht new
+     * van deze class een object maak in een variabele.
+     * We construeren hiermee het object, en op basis van de code
+     * in de method vullen we de properties gelijk met waarden
+     */
     public function __construct($routes)
     {
         $this->routes = $routes;
@@ -25,6 +38,10 @@ class RequestHandler
         $this->parseURL();
     }
 
+    /*
+     * Voordat we echt de requests gaan afhandelen willen we eerst de URL geanalyseerd hebben
+     * En de properties (= variabelen in een class) vullen met de gegevens uit de URL
+     */
     private function parseURL()
     {
         if(isset($_GET['resource'])) {
@@ -38,41 +55,50 @@ class RequestHandler
         }
     }
 
+    /*
+     * In de onderstaande method bekijken eerst welk type request we ontvangen hebben van de client app
+     * Als dat achterhaald is gaan we de juiste code uitvoeren
+     */
     public function handleRequest()
     {
         // Kijken wat voor type request is het
         switch($this->request_type) {
             case 'GET':
-                //
+                // Hier komt de code die een GET-request gaat afhandelen
                 break;
 
             case 'POST':
-                //
+                // Hier komt de code die een POST-request gaat afhandelen
                 break;
 
             case 'PUT':
-                //
+                // Hier komt de code die een PUT request gaat afhandelen
                 break;
 
             case 'PATCH':
-                //
+                // Hier komt de code die een PATCH request gaat afhandelen
                 break;
 
             case 'DELETE':
-                //
+                // Hier komt de code die een DELETE request gaat afhandelen
                 break;
 
             case 'OPTION':
+                // Dit is een request die te maken heeft met o.a. CORS
                 $this->handleOptionsRequest();
                 break;
 
             default:
-                //
+                // In alle andere gevallen gaan we hier een standaard actie uitvoeren
                 break;
 
         }
     }
 
+    /*
+     * Onderstaande method (= functie in een class) handelt de OPTIONS request af
+     * Een browser stuurt een OPTIONS request i.v.m. de CORS beveiliging
+     */
     private function handleOptionsRequest()
     {
         if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
--- api/index.php
+++ api/index.php
@@ -1,8 +1,14 @@
 <?php
 @include('vendor/autoload.php');
 
+// We halen alle routes binnen in een variabele
 $routes = @include('app/Routes/routes.php');
 
+// We maken nu een object aan van de class RequestHandler zodat we de functionaliteit
+// in deze class ook kunnen gaan gebruiken
 $requestHandler = new App\Http\RequestHandler($routes);
 
+// We maken nu gebruik van de method handleRequest in de class RequestHandler
+// We doen dit om onze API nu ook echt op te starten en het juiste werk te laten
+// doen.
 $requestHandler->handleRequest();
